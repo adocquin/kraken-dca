b1c50b4a922aab8cd560586be3546d9f19c53d59
Avel Docquin <adocquin@outlook.com>
2021-09-10 22:33:53 +0200

    KrakenDCA: Added initialize_dca method to check if pairs exist on Kraken add create DCA objects. Few fixes and improvement.

--- krakendca/krakendca.py
+++ krakendca/krakendca.py
@@ -1,15 +1,19 @@
+from typing import List
+
 from .config import Config
-from.dca import DCA
+from .dca import DCA
 from .pair import Pair
 from krakenapi import KrakenApi
 
 
-class KrakenDCA():
+class KrakenDCA:
     """
     KrakenDCA main loop encapsulation.
     """
+
     config: Config
     ka: KrakenApi
+    dcas_list: List[DCA]
 
     def __init__(self, config: Config, ka: KrakenApi):
         """
@@ -20,19 +24,27 @@ class KrakenDCA():
         """
         self.config = config
         self.ka = ka
+        self.dcas_list = []
+
+    def initialize_dca(self):
+        """
+        Check if pairs exist and initialize DCA objects List
+        """
+        print("Hi, current DCA configuration:")
+        for dca_pair in self.config.dca_pairs:
+            # Initialize the Pair object from pair specified in configuration
+            # file and data from Kraken.
+            pair = Pair.get_pair_from_kraken(self.ka, dca_pair.get("pair"))
+            # Initialize the DCA objects.
+            self.dcas_list.append(DCA(
+                self.ka, dca_pair.get("delay"), pair, dca_pair.get("amount")
+            ))
 
     def handle_dca(self):
         """
         Handle pairs Dollar Cost Averaging.
         """
-        for dca_pair in self.config.dca_pairs:
-            # Initialize the Pair object from pair specified in configuration
-            # file and data from Kraken.
-            pair = Pair.get_pair_from_kraken(self.ka, dca_pair.get("pair"))
-            # Initialize the DCA object.
-            dca = DCA(self.ka, dca_pair.get("delay"), pair, dca_pair.get("amount"))
+        # Iterate though DCA objects list and launch DCA logic.
+        for dca in self.dcas_list:
             # Execute DCA logic.
-            # dca.handle_dca_logic()
-
-
-
+            dca.handle_dca_logic()

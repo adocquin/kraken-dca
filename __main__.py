88c89372780417def8908e26d475de36e34bce3f
Avel Docquin <adocquin@outlook.com>
2022-05-09 21:14:27 +0200

    fix: Config file path relative to main.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,17 @@
+import os
+
 from krakenapi import KrakenApi
 
 from krakendca import Config, KrakenDCA
 
 if __name__ == "__main__":
     # Get parameters from configuration file.
-    config = Config("config.yaml")
+    current_directory: str = os.getcwd()
+    config_file: str = current_directory + "/config.yaml"
+    config: Config = Config(config_file)
     # Initialize the KrakenAPI object.
-    ka = KrakenApi(config.api_public_key, config.api_private_key)
+    ka: KrakenApi = KrakenApi(config.api_public_key, config.api_private_key)
     # Initialize KrakenDCA and handle the DCA based on configuration.
-    kdca = KrakenDCA(config, ka)
+    kdca: KrakenDCA = KrakenDCA(config, ka)
     kdca.initialize_pairs_dca()
     kdca.handle_pairs_dca()
